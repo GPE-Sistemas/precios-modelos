3f7953040abc9a8613c9d339b0b3670288e808da
Ignacio Martín Arano <tatukis@gmail.com>
2024-02-28 11:05:59 -0300

    Update relevamiento DTO and model

--- src/interfaces/cliente/relevamiento/relevamiento.dto.ts
+++ src/interfaces/cliente/relevamiento/relevamiento.dto.ts
@@ -1,28 +1,30 @@
-import { IPagoRelevamiento } from "./pagoRelevamiento";
-import { IProductoRelevamiento } from "./productoRelevamiento";
-import { IUbicacionRelevamiento } from "./ubicacionRelevamiento";
+import { IPagoRelevamiento } from './pagoRelevamiento';
+import { IProductoRelevamiento } from './productoRelevamiento';
+import { IUbicacionRelevamiento } from './ubicacionRelevamiento';
 
 export interface ICreateRelevamiento {
   idCliente?: string;
   idUsuario?: string;
   fecha?: string;
-  campAgricola: string;
-  condicionComercial: string;
-  fuente: string;
+  campAgricola?: string;
+  condicionComercial?: string;
+  oferente?: string;
+  distribuidor?: string;
   tipoRelevamiento?: string;
   verificado?: boolean;
   observaciones?: string;
   disponibilidad?: string;
-  ubicacion: IUbicacionRelevamiento;
-  pago: IPagoRelevamiento;
-  producto: IProductoRelevamiento;
+  ubicacion?: IUbicacionRelevamiento;
+  pago?: IPagoRelevamiento;
+  producto?: IProductoRelevamiento;
 }
 
 export interface IUpdateRelevamiento {
   fecha?: string;
   campAgricola?: string;
   condicionComercial?: string;
-  fuente?: string;
+  oferente?: string;
+  distribuidor?: string;
   tipoRelevamiento?: string;
   verificado?: boolean;
   observaciones?: string;
--- src/interfaces/cliente/relevamiento/relevamiento.model.ts
+++ src/interfaces/cliente/relevamiento/relevamiento.model.ts
@@ -1,23 +1,24 @@
-import { ICliente, IUsuario } from "../../globales";
-import { IPagoRelevamiento } from "./pagoRelevamiento";
-import { IProductoRelevamiento } from "./productoRelevamiento";
-import { IUbicacionRelevamiento } from "./ubicacionRelevamiento";
+import { ICliente, IUsuario } from '../../globales';
+import { IPagoRelevamiento } from './pagoRelevamiento';
+import { IProductoRelevamiento } from './productoRelevamiento';
+import { IUbicacionRelevamiento } from './ubicacionRelevamiento';
 
 export interface IRelevamiento {
-  _id: string;
-  idCliente: string;
-  fecha: string;
-  campAgricola: string;
-  idUsuario: string;
-  condicionComercial: string;
-  fuente: string;
-  tipoRelevamiento: string;
-  verificado: boolean;
-  observaciones: string;
-  disponibilidad: string;
-  ubicacion: IUbicacionRelevamiento;
-  pago: IPagoRelevamiento;
-  producto: IProductoRelevamiento;
+  _id?: string;
+  idCliente?: string;
+  fecha?: string;
+  campAgricola?: string;
+  idUsuario?: string;
+  condicionComercial?: string;
+  oferente?: string;
+  distribuidor?: string;
+  tipoRelevamiento?: string;
+  verificado?: boolean;
+  observaciones?: string;
+  disponibilidad?: string;
+  ubicacion?: IUbicacionRelevamiento;
+  pago?: IPagoRelevamiento;
+  producto?: IProductoRelevamiento;
   // Virtuals
   cliente?: ICliente;
   usuario?: IUsuario;
